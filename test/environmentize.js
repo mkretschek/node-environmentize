f6f9799d6fe2096b4f4682c589a4629537da280a
Mathias Kretschek <mathias@kretschek.com.br>
2013-08-28 09:23:14 +0000

    Changed e12e's API and updated tests.

--- test/environmentize.js
+++ test/environmentize.js
@@ -1,240 +1,244 @@
-var expect = require('expect.js')
-  , e12e = require('../');
+/* global describe: true, it: true, before: true, after: true */
+
+(function () {
+  'use strict';
+
+  var expect = require('expect.js'),
+    e12e = require('../');
 
 
-var ENV = process.env.NODE_ENV || 'test';
-
-
-function setEnv(env) {
-  process.env.NODE_ENV = env;
-}
-
-
-describe('environmentize', function () {
-  afterEach(function () {
-    // retorna para o ambiente original
-    process.env.NODE_ENV = ENV;
-  });
-
-  it('is accessible', function () {
-    expect(e12e).not.to.be(undefined);
-  });
-
-
-  it('is a function', function () {
-    expect(e12e).to.be.a('function');
-  });
-
-
-  it('returns the correct value if a environment-value map is given', function () {
-    var map = {
-      development : 'foo',
-      test : 'baz'
-    };
-
-    expect(e12e(map)).to.be('baz');
-
-    setEnv('development');
-    expect(e12e(map)).to.be('foo');
-  });
-
-
-  it('returns the correct value for unmatched environments when the "other" key is defined in the map', function () {
-    var map = {
-      test : 'foo',
-      other : 'bar'
-    };
-
-    expect(e12e(map)).to.be('foo');
-    
-    setEnv('production');
-    expect(e12e(map)).to.be('bar');
-
-    setEnv('development');
-    expect(e12e(map)).to.be('bar');
-  });
-
-
-  it('returns the expected value when production/staging and dev/test values are given', function () {
-    expect(e12e('foo', 'bar')).to.be('bar');
-
-    setEnv('production');
-    expect(e12e('foo', 'bar')).to.be('foo');
-  });
-
-
-  it('throws an error if only one param is passed and is not an object', function () {
-    function doIt() {
-      e12e('foo');
-    }
-
-    expect(doIt).to.throwError();
-  });
-
-
-  it('throws an error if more than two params are given', function () {
-    function doIt() { e12e('foo', 'bar', 'baz'); }
-
-    expect(doIt).to.throwError();
-  });
-
-
-  describe('#env', function () {
+  describe('environmentize', function () {
     it('is accessible', function () {
+      expect(e12e).not.to.be(undefined);
+    });
+
+
+    it('is a function', function () {
+      expect(e12e).to.be.a('function');
+    });
+
+
+    it('has a default environment set', function () {
+      expect(e12e.settings.environments).to.be.an('array');
+      expect(e12e.settings.environments).to.be.eql([
+        'development',
+        'test',
+        'integration',
+        'staging',
+        'production'
+      ]);
+    });
+
+
+    it('returns the current environment if called without arguments',
+      function () {
+        expect(e12e()).to.be(e12e.env);
+      });
+
+
+    it('returns a boolean if the first argument is a string or array',
+      function () {
+        expect(e12e(e12e.env)).to.be(true);
+        expect(e12e(['foo', e12e.env])).to.be(true);
+        expect(e12e('foo')).to.be(false);
+        expect(e12e(['foo', 'bar'])).to.be(false);
+      });
+
+
+    it('works with an object representing an env-value map', function () {
+      var foo = e12e({
+        'development' : 'development foo',
+        'production' : 'production foo'
+      }, 'other foo');
+
+      switch (e12e.env) {
+      case 'development':
+        expect(foo).to.be('development foo');
+        break;
+      case 'production':
+        expect(foo).to.be('production foo');
+        break;
+      default:
+        expect(foo).to.be('other foo');
+      }
+    });
+
+
+    it('accepts a string + value combination as params', function () {
+      var foo = e12e('production', 'production foo', 'other foo');
+
+      switch (e12e.env) {
+      case 'production':
+        expect(foo).to.be('production foo');
+        break;
+      default:
+        expect(foo).to.be('other foo');
+      }
+    });
+
+
+    it('accepts an array of strings to map a single value to multiple envs',
+      function () {
+        var foo = e12e(['staging', 'production'],
+          'staging and production foo',
+          'other foo');
+
+        switch (e12e.env) {
+        case 'production':
+          expect(foo).to.be('staging and production foo');
+          break;
+        case 'staging':
+          expect(foo).to.be('staging and production foo');
+          break;
+        default:
+          expect(foo).to.be('other foo');
+        }
+      });
+
+
+    it('if value is a function, uses its return value, not the function itself',
+      function () {
+        var foo = e12e(
+          'integration',
+          function () { return 'integration foo'; },
+          function () { return 'other foo'; }
+        );
+
+        switch (process.env.NODE_ENV) {
+        case 'integration':
+          expect(foo).to.be('integration foo');
+          break;
+        default:
+          expect(foo).to.be('other foo');
+        }
+      });
+
+
+    it('exposes an env property', function () {
       expect(e12e.env).not.to.be(undefined);
+      expect(e12e.env).to.be.a('string');
+      expect(e12e.env).to.be(process.env.NODE_ENV);
     });
 
 
-    it('returns the current environment', function () {
-      expect(e12e.env).to.be('test');
-      setEnv('production');
-      expect(e12e.env).to.be('production');
-    });
-  }); // #env
-
-  
-  describe('#isProduction()', function () {
-    it('is accessible', function () {
-      expect(e12e.isProduction).not.to.be(undefined);
+    it('exposes a settings property', function () {
+      expect(e12e.settings).not.to.be(undefined);
+      expect(e12e.settings).to.be.an('object');
     });
 
 
-    it('is a function', function () {
-      expect(e12e.isProduction).to.be.a('function');
+    describe('#setup()', function () {
+      var settingsO, settingsX;
+
+      settingsO = {
+        environments : e12e.settings.environments,
+        defaultEnv : e12e.settings.defaultEnv
+      };
+
+      settingsX = {
+        environments : ['dev', 'ci', 'prod'],
+        defaultEnv : 'dev'
+      };
+
+
+      before(function () {
+        e12e.setup(settingsX);
+      });
+
+
+      after(function () {
+        e12e.setup(settingsO);
+      });
+
+
+      it('is accessible', function () {
+        expect(e12e.setup).not.to.be(undefined);
+      });
+
+
+      it('is a function', function () {
+        expect(e12e.setup).to.be.a('function');
+      });
+
+
+      it('updates the environments list', function () {
+        expect(e12e.settings.environments).not.to.be(settingsO.environments);
+        expect(e12e.settings.environments).to.be.eql(settingsX.environments);
+      });
+
+
+      it('updates the default environment', function () {
+        expect(e12e.settings.defaultEnv).not.to.be(settingsO.defaultEnv);
+        expect(e12e.settings.defaultEnv).to.be(settingsX.defaultEnv);
+      });
+
+
+      it('removes the previous environment methods', function () {
+        var len, i;
+        for (i = 0, len = settingsO.environments.length; i < len; i += 1) {
+          expect(e12e).not.to.have.property(settingsO.environments[i]);
+        }
+      });
+
+
+      it('creates new environment methods', function () {
+        var envs, env, len, i;
+        envs = e12e.settings.environments;
+        for (i = 0, len = envs.length; i < len; i += 1) {
+          env = envs[i];
+          expect(e12e).to.have.property(env);
+          expect(e12e[env]).to.be.a('function');
+        }
+      });
     });
 
 
-    it('returns true in production environment', function () {
-      setEnv('production');
-      expect(e12e.isProduction()).to.be(true);
+    describe('environment method', function () {
+      it('is accessible', function () {
+        var envs, len, i;
+        envs = e12e.settings.environments;
+        for (i = 0, len = envs.length; i < len; i += 1) {
+          expect(e12e[envs[i]]).not.to.be(undefined);
+        }
+      });
+
+
+      it('is a function', function () {
+        var envs, len, i;
+        envs = e12e.settings.environments;
+        for (i = 0, len = envs.length; i < len; i += 1) {
+          expect(e12e[envs[i]]).to.be.a('function');
+        }
+      });
+
+
+      it('checks the current environment if no arguments are given',
+        function () {
+          var envs, env, len, i;
+          envs = e12e.settings.environments;
+          for (i = 0, len = envs.length; i < len; i += 1) {
+            env = envs[i];
+            if (env === e12e.env) {
+              expect(e12e[env]()).to.be.ok();
+            } else {
+              expect(e12e[env]()).not.to.be.ok();
+            }
+          }
+        });
+
+
+      it('returns a value if arguments are passed', function () {
+        var envs, env, len, i;
+        envs = e12e.settings.environments;
+        for (i = 0, len = envs.length; i < len; i += 1) {
+          env = envs[i];
+          if (env === e12e.env) {
+            expect(e12e[env]('foo', 'bar')).to.be('foo');
+          } else {
+            expect(e12e[env]('foo', 'bar')).to.be('bar');
+          }
+        }
+      });
     });
-
-
-    it('returns false if not in production environment', function () {
-      setEnv('other');
-      expect(e12e.isProduction()).to.be(false);
-    });
-  }); // #isProduction
-  
-  
-  describe('#isDevelopment()', function () {
-    it('is accessible', function () {
-      expect(e12e.isDevelopment).not.to.be(undefined);
-    });
-
-
-    it('is a function', function () {
-      expect(e12e.isDevelopment).to.be.a('function');
-    });
-
-
-    it('returns true in development environment', function () {
-      setEnv('development');
-      expect(e12e.isDevelopment()).to.be(true);
-    });
-
-
-    it('returns false if not in development environment', function () {
-      setEnv('other');
-      expect(e12e.isDevelopment()).to.be(false);
-    });
-  }); // #isDevelopment()
-  
-  
-  describe('#isTest()', function () {
-    it('is accessible', function () {
-      expect(e12e.isTest).not.to.be(undefined);
-    });
-
-
-    it('is a function', function () {
-      expect(e12e.isTest).to.be.a('function');
-    });
-
-
-    it('returns true in test environment', function () {
-      setEnv('test');
-      expect(e12e.isTest()).to.be(true);
-    });
-
-
-    it('returns false if not in test environment', function () {
-      setEnv('other');
-      expect(e12e.isTest()).to.be(false);
-    });
-  }); // #isTest()
-  
-  
-  describe('#isStage()', function () {
-    it('is accessible', function () {
-      expect(e12e.isStage).not.to.be(undefined);
-    });
-
-
-    it('is a function', function () {
-      expect(e12e.isStage).to.be.a('function');
-    });
-
-
-    it('returns true in stage environment', function () {
-      setEnv('stage');
-      expect(e12e.isStage()).to.be(true);
-    });
-
-
-    it('returns false if not in stage environment', function () {
-      setEnv('other');
-      expect(e12e.isStage()).to.be(false);
-    });
-  }); // #isStage()
-
-
-  describe('#setup()', function () {
-    var express = require('express');
-
-
-    it('is accessible', function () {
-      expect(e12e.setup).not.to.be(undefined);
-    });
-
-
-    it('is a function', function () {
-      expect(e12e.setup).to.be.a('function');
-    });
-
-
-    it('sets the `e12e` property in an express.js app locals', function () {
-      var app = express();
-
-      expect(app.locals).not.to.have.property('e12e');
-
-      e12e.setup(app);
-      expect(app.locals).to.have.property('e12e');
-    });
-
-
-    it('creates a simplified object in locals', function () {
-      var app = express();
-      e12e.setup(app);
-
-      // The app.locals' e12e object should not be a function
-      expect(app.locals.e12e).to.be.an('object');
-      expect(app.locals.e12e).not.to.be(e12e);
-
-      // app.locals' e12e should have an `env` object like the regular e12e
-      expect(app.locals.e12e).to.have.property('env');
-      expect(app.locals.e12e.env).to.be(e12e.env);
-
-      // app.locals' e12e should not have a setup method
-      expect(app.locals.e12e).not.to.have.property('setup');
-    });
-
-
-    it('requires an object with a locals property', function () {
-      function fail() { e12e.setup({}); }
-      expect(fail).to.throwError();
-    });
-  }); // #setup()
-
-}); // environmentize
-
+  });
+})();
