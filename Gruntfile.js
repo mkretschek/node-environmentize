dde17a7b5dc7d608c4ffc6ba901fb4befdac7378
Mathias Kretschek <mathias@kretschek.com.br>
2014-04-28 11:07:27 -0300

    Added test task to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,18 +4,33 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     jshint: {
-      options : {
+      options: {
         jshintrc: true
       },
-      all : ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'dot',
+          ui: 'bdd',
+          ignoreLeaks: false
+        },
+        src: ['test/**/*.js']
+      }
     }
   });
 
   // Load the jshint plugin
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
+  // Load the mocha runner plugin
+  grunt.loadNpmTasks('grunt-mocha-test');
+
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'mochaTest']);
 
 };
